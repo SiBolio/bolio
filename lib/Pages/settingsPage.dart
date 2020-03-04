0d741cb3b3511d24fe54641ff5f447fb9b2c72f3
Simon <simonnuber@gmail.com>
2020-03-04 20:20:05 +0100

    initial

---
+++ lib/Pages/settingsPage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:smarthome/Services/saveService.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _edited = false;
+  String _ipAdress = '';
+  SaveService saveService = new SaveService();
+
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Einstellungen'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: _loadIpAddress(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasData) {
+                _controller.text = snapshot.data;
+                return TextFormField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.network_locked),
+                    hintText: 'IP Adresse und Port vom ioBroker',
+                    labelText: 'IP Adresse',
+                  ),
+                  onChanged: (ipAdress) {
+                    _ipAdress = ipAdress;
+                    setState(() {
+                      _edited = true;
+                    });
+                  },
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: new Visibility(
+        visible: _edited,
+        child: FloatingActionButton(
+          onPressed: () {
+            _save();
+          },
+          child: Icon(Icons.save),
+        ),
+      ),
+    );
+  }
+
+  _save() {
+    saveService.saveIpAddress(_ipAdress);
+  }
+
+  Future<String> _loadIpAddress() {
+    return saveService.getIpAddress();
+  }
+}
