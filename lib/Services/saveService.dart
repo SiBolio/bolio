862496d45c2d9f8c7dda5aa50cf4f9d7e9f13fda
Simon <simonnuber@gmail.com>
2020-03-21 19:01:40 +0100

    .

--- lib/Services/saveService.dart
+++ lib/Services/saveService.dart
@@ -1,21 +1,11 @@
 import 'dart:convert';
-
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smarthome/Models/favoriteModel.dart';
 import 'package:smarthome/Models/objectsModel.dart';
 
 class SaveService {
   addObjectToFavorite(ObjectsModel object) async {
-    FavoriteModel favorite =
-        new FavoriteModel(title: object.name, id: object.id);
-    var _prefs = await SharedPreferences.getInstance();
-    _prefs.setString('1', json.encode(favorite.toJson()));
-
-//////////////////////////
-    var x = _prefs.getString('1');
-    var y = FavoriteModel.fromJson(json.decode(x));
-    print(y.id);
-//////////////////////////
+//TODO
   }
 
   saveIpAddress(String ipAddress) async {
